469dd15fe0999a7494c16505e04ecd47ae832796
Shaba Rahavi <40212946+Shaba9@users.noreply.github.com>
2019-03-06 14:26:22 -0800

    Routes and Header (#2)
    Adds in basic router and header component

---
+++ src/components/app/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from '../Home';
+import Header from '../Header';
+
+export default function App() {
+  return (
+    <Router>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/users"></Route>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </>
+    </Router> 
+  );
+}
