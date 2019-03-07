169917ba5e98335673b788d619d88b561f1cafb0
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-06 16:37:34 -0800

    get top 5 posts selector test passing

--- src/selectors/posts.js
+++ src/selectors/posts.js
@@ -1,1 +1,7 @@
 export const getAllPosts = state => state.posts.posts;
+
+export const getTop5Posts = state => {
+  return getAllPosts(state)
+    .sort((firstPost, secondPost) => secondPost.body.length - firstPost.body.length)
+    .slice(0, 5);
+};
