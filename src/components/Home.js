91dfdac40620d3ce3c0e249d6af4fe8aacada522
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-06 16:42:15 -0800

    Home component done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Home() {
-  return <h1>Home</h1>;
+export default function Home({ posts }) {
+  const postList = posts.map(post => <li key={post.id}>{post.title}</li>);
+  return <ul>{postList}</ul>;
 }
+
+Home.propTypes = {
+  posts: PropTypes.array.isRequired
+};
