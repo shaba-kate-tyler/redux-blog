c4a9960ebff40926411c10db9e9840f1ecc5fdf8
Shaba Rahavi <40212946+Shaba9@users.noreply.github.com>
2019-03-07 13:50:54 -0800

    Additional posts redux (#13)
    * getFiltered post selector test
    
    * userId added to posts
    
    * getPostForUser and test added

--- src/selectors/posts.test.js
+++ src/selectors/posts.test.js
@@ -1,7 +1,9 @@
 import {
   getAllPosts,
   getTop5Posts,
-  getPostSearchTerm
+  getPostSearchTerm,
+  getFilteredPosts,
+  getPostsForUser
 } from './posts';
 
 describe('post selectors', () => {
@@ -13,12 +15,36 @@ describe('post selectors', () => {
         postSearchTerm: 'pos',
         isLoading: false,
         posts: [
-          { body: 'a pfdsafdasfdasfdafda ost' },
-          { body: 'b pofdsaf dsafdsafdasst' },
-          { body: 'c fdsafdsa' },
-          { body: 'd poadsflk;jdaslkfjkldsafst' },
-          { body: 'c pos tffdsafd' },
-          { body: 'c post' }
+          { 
+            userId: 1,
+            title: 'a post',
+            body: 'a pfdsafdasfdasfdafda ost'
+          },
+          { 
+            userId: 2,
+            title: 'another post',
+            body: 'b pofdsaf dsafdsafdasst'
+          },
+          { 
+            userId: 3,
+            title: 'other post',
+            body: 'c fdsafdsa'
+          },
+          { 
+            userId: 4,
+            title: 'another other post',
+            body: 'd poadsflk;jdaslkfjkldsafst'
+          },
+          { 
+            userId: 5,
+            title: 'another other other post',
+            body: 'c pos tffdsafd'
+          },
+          { 
+            userId: 6,
+            title: 'another other another',
+            body: 'c post'
+          }
         ]
       }
     };
@@ -33,16 +59,78 @@ describe('post selectors', () => {
     const result = getTop5Posts(state);
     expect(result).toHaveLength(5);
     expect(result).toEqual([
-      { body: 'd poadsflk;jdaslkfjkldsafst' },
-      { body: 'a pfdsafdasfdasfdafda ost' },
-      { body: 'b pofdsaf dsafdsafdasst' },
-      { body: 'c pos tffdsafd' },
-      { body: 'c fdsafdsa' }
+      {
+        ...state.posts.posts[0],
+        title: 'another other post',
+        body: 'd poadsflk;jdaslkfjkldsafst'
+      },
+      {
+        ...state.posts.posts[1],
+        title: 'a post',
+        body: 'a pfdsafdasfdasfdafda ost'
+      },
+      {
+        ...state.posts.posts[2],
+        title: 'another post',
+        body: 'b pofdsaf dsafdsafdasst'
+      },
+      {
+        ...state.posts.posts[3],
+        title: 'another other other post',
+        body: 'c pos tffdsafd'
+      },
+      {
+        ...state.posts.posts[4],
+        title: 'other post',
+        body: 'c fdsafdsa'
+      }
     ]);
   });
 
   it('gets post search term', () => {
     const result = getPostSearchTerm(state);
     expect(result).toEqual('pos');
+  });
+
+  it('gets filtered posts by title', () => {
+    const result = getFilteredPosts(state);
+    expect(result).toEqual([
+      { 
+        ...state.posts.posts[0],
+        title: 'a post',
+        body: 'a pfdsafdasfdasfdafda ost'
+      },
+      { 
+        ...state.posts.posts[1],
+        title: 'another post',
+        body: 'b pofdsaf dsafdsafdasst'
+      },
+      { 
+        ...state.posts.posts[2],
+        title: 'other post',
+        body: 'c fdsafdsa'
+      },
+      { 
+        ...state.posts.posts[3],
+        title: 'another other post',
+        body: 'd poadsflk;jdaslkfjkldsafst'
+      },
+      { 
+        ...state.posts.posts[4],
+        title: 'another other other post',
+        body: 'c pos tffdsafd'
+      },
+    ]);
+  });
+
+  it('gets post for user', () => {
+    const result = getPostsForUser(state, 1);
+    expect(result).toEqual([
+      {
+        userId: 1,
+        title: 'a post',
+        body: 'a pfdsafdasfdasfdafda ost'
+      }
+    ]);
   });
 });
