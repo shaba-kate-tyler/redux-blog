266fac6079a0db2be1c0f8204dbb2966953aef49
Shaba Rahavi <40212946+Shaba9@users.noreply.github.com>
2019-03-07 11:34:42 -0800

    Posts redux setup (#11)
    * search term added to ars
    
    * update search term reducer test
    
    * post search term selector test
    
    * update search term action test
    
    * load post start and end test

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,7 +1,20 @@
 import { getPosts } from '../services/blogApi';
 
+export const LOAD_POSTS_START = 'LOAD_POSTS_START';
+export const LOAD_POSTS_END = 'LOAD_POSTS_END';
+
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const fetchPosts = () => ({
   type: FETCH_POSTS,
+  loadStart: LOAD_POSTS_START,
+  loadEnd: LOAD_POSTS_END,
   payload: getPosts()
+});
+
+export const UPDATE_POST_SEARCH_TERM = 'UPDATE_POST_SEARCH_TERM';
+export const updatePostSearchTerm = term => ({
+  type: UPDATE_POST_SEARCH_TERM,
+  loadStart: LOAD_POSTS_START,
+  loadEnd: LOAD_POSTS_END,
+  payload: term
 });
--- src/reducers/posts.test.js
+++ src/reducers/posts.test.js
@@ -9,9 +9,11 @@ describe('reducer', () => {
     };
   });
 
-  it('it updates posts state with an array of all posts', () => {
+  it('updates posts state with an array of all posts', () => {
     const action = {
       type: 'FETCH_POSTS',
+      postSearchTerm: 'a',
+      isLoading: false,
       payload: [
         { a: 'a post' }
       ]
@@ -21,6 +23,40 @@ describe('reducer', () => {
     expect(results).toEqual({
       ...state,
       posts: [{ a: 'a post' }]
+    });
+  });
+
+  it('updates post search term', () => {
+    const action = {
+      type: 'UPDATE_POST_SEARCH_TERM',
+      payload: 'pos'
+    };
+
+    expect(reducer(state, action)).toEqual({
+      ...state,
+      postSearchTerm: 'pos'
+    });
+  });
+
+  it('loads post start', () => {
+    const action = {
+      type: 'LOAD_POSTS_START'
+    };
+
+    expect(reducer(state, action)).toEqual({
+      ...state,
+      isLoading: true
+    });
+  });
+
+  it('loads post end', () => {
+    const action = {
+      type: 'LOAD_POSTS_END'
+    };
+
+    expect(reducer(state, action)).toEqual({
+      ...state,
+      isLoading: false
     });
   });
 });
