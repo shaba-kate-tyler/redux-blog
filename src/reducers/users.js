811eb02cd64c5f1f2edf53858585fd20b502af62
Tyler Corbett <43227257+tylercorbett@users.noreply.github.com>
2019-03-07 10:45:24 -0800

    users selector test passing (#9)

---
+++ src/reducers/users.js
@@ -0,0 +1,30 @@
+import { 
+  FETCH_USERS, 
+  LOAD_USERS_START,
+  LOAD_USERS_END 
+} from '../actions/users';
+
+const initialState = {
+  users: [],
+  isLoading: false
+};
+
+export default function reducer(state = initialState, { type, payload }) {
+  switch(type) {
+    case FETCH_USERS:
+      return {
+        ...state,
+        users: payload
+      };
+    case LOAD_USERS_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case LOAD_USERS_END:
+      return {
+        ...state,
+        isLoading: false
+      };
+  }
+}
