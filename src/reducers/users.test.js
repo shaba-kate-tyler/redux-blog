ddda22ec04f3489103d14376f40178f66bcd1f83
Kate Dameron <hello@katedameron.com>
2019-03-07 11:31:48 -0800

    Redux user tests (#10)
    * green test for fetchUsers actionCreator
    
    * green test for users reducer receiving the FETCH_USERS action
    
    * green test for passing LOAD_USERS_START action to users reducer
    
    * green test for passing LOAD_USERS_END action to users reducer

---
+++ src/reducers/users.test.js
@@ -0,0 +1,146 @@
+import reducer from './users';
+
+describe('users reducer', () => {
+  let state = {};
+
+  beforeEach(() => {
+    state = {
+      users: [],
+      isLoading: false
+    };
+  });
+
+  it('it updates users state with an array of all users and passes actions through the promise middleware to update isLoading as false', () => {
+    const action = {
+      type: 'FETCH_USERS',
+      loadStart: 'LOAD_USERS_START',
+      loadEnd: 'LOAD_USERS_END',
+      payload: [
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+              lat: '-37.3159',
+              lng: '81.1496'
+            }
+          },
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+          company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+          }
+        },
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+              lat: '-37.3159',
+              lng: '81.1496'
+            }
+          },
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+          company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+          }
+        }
+      ]
+    };
+
+    const results = reducer(state, action);
+    expect(results).toEqual({
+      ...state,
+      users: [
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+              lat: '-37.3159',
+              lng: '81.1496'
+            }
+          },
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+          company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+          }
+        },
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+              lat: '-37.3159',
+              lng: '81.1496'
+            }
+          },
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+          company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+          }
+        }
+      ],
+      isLoading: false
+    });
+  });
+
+  it('it updates isLoading state as true', () => {
+    const action = {
+      type: 'LOAD_USERS_START'
+    };
+
+    const result = reducer(state, action);
+    expect(result).toEqual({
+      ...state,
+      isLoading: true
+    });
+  });
+
+  it('it updates isLoading state as false', () => {
+    const action = {
+      type: 'LOAD_USERS_END'
+    };
+
+    const result = reducer(state, action);
+    expect(result).toEqual({
+      ...state,
+      isLoading: false
+    });
+  });
+});
