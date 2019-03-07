66f409e403281082571811399ff282389d4398ed
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-07 10:15:58 -0800

    Home snapshot test added and passing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Posts from './posts/Posts';
 
 export default function Home({ posts }) {
-  const postList = posts.map(post => <li key={post.id}>{post.title}</li>);
-  return <ul>{postList}</ul>;
+  return <Posts posts={posts}/>;
 }
 
 Home.propTypes = {
