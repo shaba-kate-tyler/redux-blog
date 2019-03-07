7d4529adb4858e54303c154745742405955eb3f7
Kate Dameron <katerj33@gmail.com>
2019-03-07 09:18:39 -0800

    User component and snapshot

---
+++ src/components/User.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function User({ name, username, website, email }) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <h4>@{username}</h4>
+      <p>{website} / {email}</p>
+    </div>
+  );
+}
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  website: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired
+};
+
+export default User;
