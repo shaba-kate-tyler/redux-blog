5d9cc03d0a1c06e18cf576ce5dd4a00b49bce228
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-08 09:23:05 -0800

    styles linked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './styles/Header.css';
 
 export default function Header() {
   return (
-      <>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-      </>
+    <div className={styles.Header}>
+      <Link to="/">Home</Link>
+      <Link to="/users">Users</Link>
+    </div>
   );
 }
